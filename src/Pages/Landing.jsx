3436d43d6297b53880278de41d2678b132f68d51
Kevin <kevinator900@gmail.com>
2021-08-31 14:12:31 -0700

    added desktop landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,28 +3,36 @@ import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="landing">
-      <h1>RV AC Repairs</h1>
-      <h2>We live in a society</h2>
-      <div className="img-container">
-        <img
-          src="https://picsum.photos/260/130?grayscale"
-          alt="Landing Picture"
-        />
-      </div>
-      <div className="about-container">
-        <h3 className="about-title">About</h3>
-        <p className="about">
-          Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa excepteur
-          nisi nisi. Laborum consectetur id ex fugiat ad amet Lorem.
-        </p>
+    <>
+      <div className="landing">
+        <div className="title-container">
+          <h1>RV AC Repairs</h1>
+          <h2>We live in a hot state</h2>
+        </div>
+
+        <div className="big-container">
+          <div className="img-container">
+            <img
+              src="https://picsum.photos/500/250?grayscale"
+              alt="Landing Picture"
+            />
+          </div>
+          <div className="about-container">
+            <h3 className="about-title">About</h3>
+            <p className="about">
+              Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa
+              excepteur nisi nisi. Laborum consectetur id ex fugiat ad amet
+              Lorem.
+            </p>
+          </div>
+        </div>
       </div>
       <div className="contact-container">
         <Link to="/contact" className="contact">
           CONTACT ME
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
