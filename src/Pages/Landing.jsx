2341b87a4bb2c91842de9fd0c0476977c06d7481
Kevin <kevinator900@gmail.com>
2021-08-31 13:58:24 -0700

    finished landing layout

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -6,10 +7,23 @@ const About = () => {
       <h1>RV AC Repairs</h1>
       <h2>We live in a society</h2>
       <div className="img-container">
-        <img src="https://picsum.photos/260/130?grayscale" alt="Landing Picture" />
+        <img
+          src="https://picsum.photos/260/130?grayscale"
+          alt="Landing Picture"
+        />
       </div>
-        <h3 className = "about-title">About</h3>
-        <p className = "about">Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa excepteur nisi nisi. Laborum consectetur id ex fugiat ad amet Lorem.</p>
+      <div className="about-container">
+        <h3 className="about-title">About</h3>
+        <p className="about">
+          Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa excepteur
+          nisi nisi. Laborum consectetur id ex fugiat ad amet Lorem.
+        </p>
+      </div>
+      <div className="contact-container">
+        <Link to="/contact" className="contact">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
   );
 };
