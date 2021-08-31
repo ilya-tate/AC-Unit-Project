749789b06951e24d0fec00ab5afd94403ffa20d7
jhunt646 <jhunt646@west-mec.org>
2021-08-31 13:27:05 -0700

    Very Cool Navbar Pog

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="landing">
       <h1>RV AC Repairs</h1>
-      <h2>We live in a hot state</h2>
+      <h2>We live in a society</h2>
       <div className="img-container">
         <img src={rick} alt="rick" />
       </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./Styles/main.css";
+import "./Styles/main.css"
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
