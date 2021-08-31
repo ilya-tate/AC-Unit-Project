4f1f2ce94e4d7fb103169c019986084a379b22b4
Kevin <kevinator900@gmail.com>
2021-08-31 13:26:28 -0700

    landing page done

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import rick from "../util/rick.png";
 
 const About = () => {
   return (
@@ -7,8 +6,10 @@ const About = () => {
       <h1>RV AC Repairs</h1>
       <h2>We live in a hot state</h2>
       <div className="img-container">
-        <img src={rick} alt="rick" />
+        <img src="https://picsum.photos/260/130?grayscale" alt="Landing Picture" />
       </div>
+        <h3 className = "about-title">About</h3>
+        <p className = "about">Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa excepteur nisi nisi. Laborum consectetur id ex fugiat ad amet Lorem.</p>
     </div>
   );
 };
