12a24faba9964a9e042f4c4a39dfdacaf7dbd906
Kevin <kevinator900@gmail.com>
2021-08-31 15:22:30 -0700

    worked more on landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -10,27 +10,31 @@ const About = () => {
           <h2>We live in a hot state</h2>
         </div>
 
-        <div className="big-container">
-          <div className="img-container">
-            <img
-              src="https://picsum.photos/500/250?grayscale"
-              alt="Landing Picture"
-            />
-          </div>
-          <div className="about-container">
-            <h3 className="about-title">About</h3>
-            <p className="about">
-              Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa
-              excepteur nisi nisi. Laborum consectetur id ex fugiat ad amet
-              Lorem.
-            </p>
-          </div>
+        <div className="img-container">
+          <img
+            src="https://picsum.photos/500/300?grayscale"
+            alt="Landing Picture"
+          />
         </div>
-      </div>
-      <div className="contact-container">
-        <Link to="/contact" className="contact">
-          CONTACT ME
-        </Link>
+        <div className="about-container">
+          <h3 className="title about-title">About</h3>
+          <p className="about">
+            Est do cillum elit sunt culpa Lorem. Eu ex do minim culpa excepteur
+            nisi nisi. Laborum consectetur id ex fugiat ad amet Lorem.
+          </p>
+        </div>
+        <div className="experience-container">
+          <h3 className="experience-title">Experience</h3>
+          <ul>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+          </ul>
+        </div>
+        <div className="contact-container">
+          <Link to="/contact" className="contact">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
     </>
   );
