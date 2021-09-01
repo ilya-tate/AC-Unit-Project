0b02a1d9b33ad6ae68ec233e781da6cdb0c4baa0
Kevin <kevinator900@gmail.com>
2021-09-01 14:05:29 -0700

    made navbar more responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import logo from "../util/logo.svg";
 import Links from "./Links";
 
 const Navbar = () => {
+  const [size, setSize] = useState(window.innerWidth);
   const [showLinks, setShowLinks] = useState(false);
+
+  const changeSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", changeSize);
+    return () => {
+      window.removeEventListener("resize", changeSize);
+    };
+  });
 
   return (
     <nav className="navbar">
       <div className="nav-center">
-        <button onClick={() => setShowLinks(!showLinks)} className="nav-toggle">
-          <GiHamburgerMenu />
-        </button>
-        <h1>
-          RV AC Repair
-        </h1>
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-        <div className={showLinks ? "showing sliding" : "sliding"} >
-          <button onClick={() => setShowLinks(!showLinks)} className="nav-toggle">
+        {size < 768 && (
+          <button
+            onClick={() => setShowLinks(!showLinks)}
+            className="nav-toggle"
+          >
             <GiHamburgerMenu />
           </button>
-          <Links />
-        </div>
+        )}
+        {size > 768 && <h1>RV AC Repair</h1>}
+
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className={showLinks || size > 768 ? "showing sliding" : "sliding"}>
+        <button onClick={() => setShowLinks(!showLinks)} className="nav-toggle">
+          {size < 768 && <GiHamburgerMenu />}
+        </button>
+        <Links />
+      </div>
     </nav>
   );
 };
